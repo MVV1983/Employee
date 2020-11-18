cd2db737e47aa77a9dca22155b318b5e99d08ccf
Valentin <51223584+MVV1983@users.noreply.github.com>
2020-11-18 14:21:29 +0700

    Add files via upload

---
+++ src/Developer.java
@@ -0,0 +1,20 @@
+public class Developer implements Employee {
+
+    private String name;
+    private String job;
+
+    public Developer(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void coffeeTime() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee dev = new Developer("Valentin", "IT");
+
+
+    }
+}
