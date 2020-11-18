70369fb268b199b58fdaec77003dc457930af279
Valentin <51223584+MVV1983@users.noreply.github.com>
2020-11-18 14:39:39 +0700

    add dev2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Employee dev = new Developer("Valentin", "IT");
-
+        Employee dev2 = new Developer("Irina", "Frontend");
+        dev.work();
 
     }
 }
